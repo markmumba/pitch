e36122a250e2c561f1df76774227bb278d74af66
markmumba <markmumba01@gmail.com>
2019-08-04 19:55:34 +0300

    Done with dummy

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import  create_app, db
 from flask_script import  Manager, Server
 from app.models import  Category, User, Peptalk, Comments
-from  flask_migrate import Migrate, MigrateCommand
+from flask_migrate import  Migrate, MigrateCommand
 from flask_login import LoginManager
 
 
