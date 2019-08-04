a95a529f3a9f4b2001ff13156dc86a0a2c96e962
markmumba <markmumba01@gmail.com>
2019-08-04 13:13:26 +0300

    Done with dummy

--- pitch.py
+++ pitch.py
@@ -1,12 +1,42 @@
 from flask import Flask,render_template,url_for,flash,redirect
 from forms import RegistrationForm,LoginForm
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'markian'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+
+
+
+
+
 
 pitches = [
     {
@@ -36,16 +66,22 @@ def about():
 
 @app.route("/register", methods = ['GET','POST'])
 def register():
-    form =RegistrationForm()
+    form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}','success')
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form = form)
+    return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
 def Login():
-    form=LoginForm
-    return render_template('Login.html', title='Login' ,form = form)
+    form=LoginForm()
+    if form.email.data == 'markmumba01@gmail.com' and form.password.data =='password':
+        flash('You have been logged in!' , 'success')
+        return redirect (url_for('home'))
+    else:
+        flash('cannot')
+
+    return render_template('login.html', title='Login' ,form = form)
 
 
 
