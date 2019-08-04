07fa031234706854786bcc3b8442a72efef26ec9
markmumba <markmumba01@gmail.com>
2019-08-04 13:52:17 +0300

    Done with dummy

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,22 @@
-from flask import render_template, url_for, flash, redirect
-from flaskblog import app
-from flaskblog.forms import RegistrationForm, LoginForm
-from flaskblog.models import User, Post
+from app.models import User,Post
+from flask import ,render_template,url_for,flash,redirect
+from app.forms import RegistrationForm,LoginForm
 
 
-posts = [
+
+
+
+pitches = [
     {
         'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
+        'title': 'Pitch Idea 1',
+        'content': 'Pitch idea itself',
         'date_posted': 'April 20, 2018'
     },
     {
         'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
+        'title': 'Pitch idea 2',
+        'content': 'Pitch idea itselft',
         'date_posted': 'April 21, 2018'
     }
 ]
@@ -23,15 +25,14 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html', pitches=pitches)
 
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-
-@app.route("/register", methods=['GET', 'POST'])
+@app.route("/register", methods = ['GET','POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -39,14 +40,19 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
+@app.route("/login")
+def Login():
+    form=LoginForm()
+    if form.email.data == 'markmumba01@gmail.com' and form.password.data =='password':
+        flash('You have been logged in!' , 'success')
+        return redirect (url_for('home'))
+    else:
+        flash('cannot')
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return render_template('login.html', title='Login' ,form = form)
+
+
+
+
+
+
